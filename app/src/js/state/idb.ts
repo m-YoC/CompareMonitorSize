a0ff1953df7ed6cbdbde6779d9fb1a1cf98ccd65
m-YoC <67212285+m-YoC@users.noreply.github.com>
2023-02-17 17:19:31 +0900

    build prod

--- app/src/js/state/idb.ts
+++ app/src/js/state/idb.ts
@@ -11,9 +11,7 @@ interface BoxDB extends DBSchema {
 
 const dbPromise = openDB<BoxDB>("boxDB-idb-store", 1.1, {
     upgrade(db) {
-        db.createObjectStore("boxes", {
-            autoIncrement: true
-        });
+        db.createObjectStore("boxes");
     }
 });
 
